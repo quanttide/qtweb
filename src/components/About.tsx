a8ac8725f1ae8c4d0b742cab57ec2db0335686e8
Mhenwa <1986535482@qq.com>
2024-05-10 16:52:07 +0800

    feat:添加手机版侧边导航栏

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,42 +2,46 @@ import Navigation from './Navigation';
 import { Helmet } from "react-helmet";
 import './assets/CSS/Page_global.css';
 import Footer from './Footer';
+import './assets/CSS/About.css'
 
-function About() {
+export default function About() {
   return (
     <div>
-      <Helmet>
-        <title>QuantTide 量潮科技</title>
-      </Helmet>
+      <Helmet><title>QuantTide 量潮科技</title></Helmet>
+
       <Navigation />
+
+      <HeaderSlider />
+
       <div>
-        {/* 公司简介页面内容 */}
         <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
-        <h1>公司简介</h1>
+        <p>一段公司简介</p>
+        <p>一段公司简介</p>
+        <p>一段公司简介</p>
+        <p>一段公司简介</p>
+        <p>一段公司简介</p>
+        <p>一段公司简介</p>
+        <p>一段公司简介</p>
+        <p>一段公司简介</p>
+        
       </div>
+
       <Footer />
     </div>
   );
 }
 
-export default About;
+function HeaderSlider() {
+  return (
+    <div className='header-slider'>
+      <img src={require('./assets/images/about_background.jpg')} alt="" />
+      <div className="caption">
+        <h1>量潮科技</h1>
+        <p>“量”寓意“定量”</p>
+        <p>“潮”寓意时代的潮流</p>
+        <p>我们取名“量潮科技”，是因为</p>
+        <p>我们致力于成为定量分析时代潮流的引领者</p>
+      </div>
+    </div>
+  );
+}
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,10 +2,14 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect, useCallback } from 'react';
 import './assets/CSS/Navigation.css';
 
-function Navigation() {
+export default function Navigation() {
+
+  //下滑菜单栏消失，上滑出现
   const [showNav, setShowNav] = useState(true);
   const [scrollPos, setScrollPos] = useState(0);
-
+  //手机版侧边导航栏
+  const [menuOpen, setMenuOpen] = useState(false);
+  
   const handleScroll = useCallback(() => {
     const currentScrollPos = window.pageYOffset;
     setShowNav(scrollPos >= currentScrollPos);
@@ -19,40 +23,60 @@ function Navigation() {
     };
   }, [handleScroll]);
 
-const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-const handleResize = useCallback(() => {
-  setWindowWidth(window.innerWidth);
-}, []);
+  //手机版导航栏
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const handleResize = useCallback(() => {
+    setWindowWidth(window.innerWidth);
+  }, []);
 
-useEffect(() => {
-  window.addEventListener('resize', handleResize);
-  return () => {
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
       window.removeEventListener('resize', handleResize);
-  };
-}, [handleResize]);
+    };
+  }, [handleResize]);
 
-return (
-  <nav style={{ top: showNav ? '0' : '-70px' }}>
-    {windowWidth <= 800 && <div className='menu'>此处有Menu</div>}
-    <div className='icon'>
-      <a href="/">
-        <img src={require('./assets/images/logo200.png')} alt='Logo' width={50} height={50} />
-      </a>
-      <span style={{marginLeft:'10px'}}>QuantTide</span>
-    </div>
-    {windowWidth > 800 && (
-      <ul>
-        <li><Link to="/">公司简介</Link></li>
-        <li><Link to="/business">业务体系</Link></li>
-        <li><Link to="/development">成长历程</Link></li>
-        <li><Link to="/culture">企业文化</Link></li>
-        <li><Link to="/news">公司新闻</Link></li>
-        <li><Link to="/joinus">加入我们</Link></li>
-        <li><Link to="/team">管理团队</Link></li>
-      </ul>
-    )}
-  </nav>
-);
+  return (
+    <nav style={{ top: showNav ? '0' : '-70px' }}>
+
+      {windowWidth <= 768 && (
+        <button className='menu' type='button' onClick={() => setMenuOpen(!menuOpen)}>
+          点击菜单
+        </button>
+      )}
+      <div className='icon'>
+        <a href="/">
+          <img src={require('./assets/images/logo200.png')} alt='Logo' width={50} height={50} />
+        </a>
+        <span style={{marginLeft:'10px'}}>QuantTide</span>
+      </div>
+      {windowWidth > 768 && (
+        <ul className='list'>
+          <li><Link to="/">公司简介</Link></li>
+          <li><Link to="/business">业务体系</Link></li>
+          <li><Link to="/development">成长历程</Link></li>
+          <li><Link to="/culture">企业文化</Link></li>
+          <li><Link to="/news">公司新闻</Link></li>
+          <li><Link to="/joinus">加入我们</Link></li>
+          <li><Link to="/team">管理团队</Link></li>
+        </ul>
+      )}
+      {menuOpen && windowWidth <= 768 && (
+          <div className={`menu-items ${menuOpen ? 'open' : ''}`}>
+            <button className='menu' type='button' onClick={() => setMenuOpen(!menuOpen)}>
+              关闭菜单
+            </button>
+            <ul className='menu-list'>
+             <li><Link to="/">公司简介</Link></li>
+              <li><Link to="/business">业务体系</Link></li>
+              <li><Link to="/development">成长历程</Link></li>
+              <li><Link to="/culture">企业文化</Link></li>
+              <li><Link to="/news">公司新闻</Link></li>
+              <li><Link to="/joinus">加入我们</Link></li>
+              <li><Link to="/team">管理团队</Link></li>
+            </ul>
+          </div>
+      )}
+    </nav>
+  );
 }
-
-export default Navigation;

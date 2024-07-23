395bafb7d35b739c80778ddb5e3ffd48b29767fb
Mhenwa <1986535482@qq.com>
2024-07-23 09:47:05 +0800

    chore: 更新了底栏信息

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -1,7 +1,13 @@
 import GithubLogo from './assets/images/github.svg';
 import './assets/CSS/Footer.css'
+import ZhihuLog from './assets/images/zhihu.svg'
 
-function Footer() {
+/**
+ * 页脚组件
+ * @returns 
+ */
+export default function Footer() {
+    // 使用的原版页脚 https://github.com/quanttide/qtweb-flutter
     return (
         <div className="footer">
 
@@ -9,21 +15,36 @@ function Footer() {
 
                 <div className="contact-us">
                     <h3>联系我们</h3>
-                    <p>官方邮箱：we@xmucpp.com</p>
-                    <p>数据服务：services@xmucpp.com</p>
+                    <p>官方邮箱：we@quanttide.com</p>
+                    <p>科研服务：research@quanttide.com</p>
                 </div>
 
                 <div className="attention-us">
                     <h3>关注我们</h3>
-                    <img src={require('./assets/images/WeChat qr code.jpg')} className="qr" alt="WeChat QR Code" width={100} height={100}/>
+                    <div className='attention-us-content'>
+                        <div>
+                            <img src={require('./assets/images/WeChatQR.png')} className="qr" alt="WeChat QR Code" width={100} height={100} />
+                            <p>微信公众号</p>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="more-links">
                     <h3>更多链接</h3>
-                    <a href="https://github.com/quanttide">
-                        <img src={GithubLogo} alt="Github" width={50} height={50}/>
-                        <p>Github</p>
-                    </a>
+                    <div className='more-links-content'>
+                        <a href="https://github.com/quanttide" target='blank'>
+                            <img src={GithubLogo} alt="Github" width={50} height={50} />
+                            <p>Github</p>
+                        </a>
+                        <a href="https://www.zhihu.com/org/liang-chao-ke-ji" target='blank'>
+                            <img src={ZhihuLog} alt="Zhuhu" width={50} height={50} />
+                            <p>知乎机构号</p>
+                        </a>
+                        <a href="https://www.zhihu.com/people/zhang_guo" target='blank'>
+                            <img src={require('./assets/images/Zhihui_iGuo.png')} alt="Zhihu_iGuo" width={50} height={50} style={{ borderRadius: '50px' }} />
+                            <p>知乎：iGuo</p>
+                        </a>
+                    </div>
                 </div>
 
             </div>
@@ -35,5 +56,3 @@ function Footer() {
         </div>
     );
 }
-  
-export default Footer;

9b3703074d32c6407597a12b30884551365a69b6
Mhenwa <1986535482@qq.com>
2024-07-23 09:41:49 +0800

    docs:添加注释

--- src/pages/components/CommonSlider.tsx
+++ src/pages/components/CommonSlider.tsx
@@ -6,15 +6,21 @@ import "./assets/CSS/CommonSlider.css"
 // 定义props类型，其中components是React函数组件的数组
 interface CommonSliderProps {
     components: FunctionComponent<any>[];
-    title:string;
-    link:string;
+    title: string;
+    link: string;
 }
 
-export default function CommonSlider({ components, title, link}:CommonSliderProps){
+/**
+ * 通用的轮播组件，轮播对象是传入的组件
+ * @param components - 需要轮播的组件
+ * @param title - 整个CommonSlider的标题
+ * @param link - 详细按钮跳转链接
+ */
+export default function CommonSlider({ components, title, link }: CommonSliderProps) {
     // 轮播设置
     const settings = {
         dots: true, // 显示点导航
-        dotsClass:'common-dots', // 自定义指示器的样式
+        dotsClass: 'common-dots', // 自定义指示器的样式
         infinite: true, // 无限循环
         speed: 500, // 切换速度
         slidesToShow: 1, // 每次显示一个子组件
--- src/pages/components/ContentSections.tsx
+++ src/pages/components/ContentSections.tsx
@@ -1,4 +1,4 @@
-import './assets/CSS/ContentSections.css'; 
+import './assets/CSS/ContentSections.css';
 import React, { useState } from 'react';
 
 interface Section {
@@ -13,8 +13,19 @@ interface ContentSectionProps {
   maintitle: string;
   sections: Section[];
 }
-
-export default function ContentSections({maintitle, sections}:ContentSectionProps){
+/**
+ * 一个分章节的文字显示组件
+ * 
+ * @param maintitle - 整个组件的标题
+ * @param sections - 章节数组
+ * @param sections.title - 本章节标题
+ * @param section.description - 本章内容描述
+ * @param section.imgSrc - 本章的展示图
+ * @param section.link - 决定详细按钮作用，link不为空，则点击后跳转至链接，否则展开详细
+ * @param section.DetailComponent - 被展开的详细内容，一个组件
+ */
+export default function ContentSections({ maintitle, sections }: ContentSectionProps) {
+  //记录详细按钮展开状态
   const [expandedSections, setExpandedSections] = useState<boolean[]>(Array(sections.length).fill(false));
 
   const toggleSection = (index: number) => {
@@ -25,6 +36,7 @@ export default function ContentSections({maintitle, sections}:ContentSectionProp
     });
   };
 
+  //决定详细按钮的动作
   const handleButtonClick = (index: number, link: string) => {
     if (link) {
       window.open(link, '_blank');

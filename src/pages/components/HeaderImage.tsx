396d142e3d175088ae625e4b9b80e887ca8275e0
Mhenwa <1986535482@qq.com>
2024-07-23 09:51:21 +0800

    feat:新增头图大小控制功能

--- src/pages/components/HeaderImage.tsx
+++ src/pages/components/HeaderImage.tsx
@@ -1,26 +1,29 @@
 import './assets/CSS/HeaderSlider.css'
 
 interface Props {
-    title: string;
-    imageUrl: string;
-    introduces: string[];
+  title: string;
+  imageUrl: string;
+  imageHeight: 'full' | 'half' | 'quarter';
+  introduces: string[];
 }
-
-export default function HeaderSlider({title,imageUrl,introduces}:Props) {
-  /*
-    页面头图组件
-  */
-    return (
-      <div className='header-slider'>
-        <img src={require(`${imageUrl}`)} alt="" />
-        <div className="caption">
-          <h1>{title}</h1>
-          {introduces.map((introduce) => (
-            <p>{introduce}</p>
-          ))}
-        </div>
+/**
+ * 页面头图组件
+ * @param title - 标题
+ * @param imageUrl - 图片地址
+ * @param imageHeight - 图片高度：整个屏幕 full | 半屏 half | 四分之一 quarter
+ * @param introduce - 详细若干行介绍
+ * @returns 
+ */
+export default function HeaderSlider({ title, imageUrl, imageHeight, introduces }: Props) {
+  return (
+    <div className={`header-slider ${imageHeight}`}>
+      <img src={require(`${imageUrl}`)} alt="" />
+      <div className="caption">
+        <h1>{title}</h1>
+        {introduces.map((introduce, index) => (
+          <p key={index}>{introduce}</p>
+        ))}
       </div>
-    );
-  }
-  
-  
+    </div>
+  );
+}
